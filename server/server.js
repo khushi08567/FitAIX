f2f039f99766be0049570abb9d993e99511bae5d
Khushi Solanki <khushi08567@users.noreply.github.com>
2026-07-28 18:10:39 +0530

    feat: add password reset web interface

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Enable CORS and JSON parsing
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // Connect Database
 connectDB();
